13e91aec1288edce2ed4ec2ce8842c124dc7c243
Julian Gruber <julian@juliangruber.com>
2017-12-03 12:57:37 +0100

    refactor selectors

--- onload.js
+++ onload.js
@@ -1,7 +1,12 @@
+const selectors = [
+  '.ProfileTweet-actionCount',
+  '.ProfileNav-value',
+  '.ProfileCardStats-statValue',
+  '.js-stat-count strong'
+]
+
 document.addEventListener('DOMContentLoaded', () => {
   const style = document.createElement('style')
-  style.appendChild(document.createTextNode(
-    '.ProfileTweet-actionCount, .ProfileNav-value, .ProfileCardStats-statValue, .js-stat-count strong { display: none; }'
-  ))
+  style.appendChild(document.createTextNode(`${selectors.join(', ')} { display: none; }`))
   document.head.appendChild(style)
 })
