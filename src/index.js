0dae49b1b86616a0a18bf122793c1e22edc09052
Julian Gruber <julian@juliangruber.com>
2017-12-06 10:07:49 +0100

    remove "who to follow"

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ const selectors = [
   '.ProfileNav-value',
   '.ProfileCardStats-statValue',
   '.js-stat-count strong',
-  '.stats .stat-count strong'
+  '.stats .stat-count strong',
+  '.wtf-module'
 ]
 
 document.addEventListener('DOMContentLoaded', () => {
   const style = document.createElement('style')
-  style.appendChild(document.createTextNode(`${selectors.join(', ')} { display: none }`))
+  style.appendChild(document.createTextNode(`${selectors.join(', ')} { display: none !important }`))
   document.head.appendChild(style)
 })
